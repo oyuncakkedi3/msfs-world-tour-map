2672c58c4704d91c8293f73bef7155187ac2852e
oyuncakkedi3 <155076976+oyuncakkedi3@users.noreply.github.com>
2025-09-24 19:13:36 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+const firebaseConfig = {
+  apiKey: "SENIN_API_KEYIN",
+  authDomain: "proje-id.firebaseapp.com",
+  projectId: "proje-id",
+  storageBucket: "proje-id.appspot.com",
+  messagingSenderId: "1234567890",
+  appId: "1:1234567890:web:abcdef123456"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
 console.log('app.js yüklendi (test sürümü)');
 
 const map = L.map('map', { worldCopyJump: true }).setView([41.015137, 28.97953], 4);
@@ -5,3 +17,4 @@ const map = L.map('map', { worldCopyJump: true }).setView([41.015137, 28.97953],
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '© OpenStreetMap katkıcıları'
 }).addTo(map);
+
